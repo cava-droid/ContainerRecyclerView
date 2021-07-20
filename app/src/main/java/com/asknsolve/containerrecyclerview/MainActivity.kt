b7526f3a22c35d13025006d220ee8ce705785229
cava <cava@kaist.ac.kr>
2021-07-20 10:27:09 +0900

    Initial commit

---
+++ app/src/main/java/com/asknsolve/containerrecyclerview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.asknsolve.containerrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loadData(): MutableList<Memo>{
+        val data: MutableList<Memo> = mutableListOf()
+        for(no in 1..100){
+            val title = "이것이 안드로이드다 ${no}"
+            val date = System.currentTimeMillis()
+        }
+        return data
+    }
+}
